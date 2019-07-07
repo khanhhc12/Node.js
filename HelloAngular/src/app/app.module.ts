62b07b12a9ae852fe8e5361d6277b6f197372d77
khanhhc12 <khanhhc12@users.noreply.github.com>
2019-07-07 08:30:45 +0700

    component

--- HelloAngular/src/app/app.module.ts
+++ HelloAngular/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { RouterModule } from '@angular/router';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    TopBarComponent,
+    ProductAlertsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
